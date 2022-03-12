f39dfb56859bf1d3339adb5aeb0df68359776c5e
meneguinha <felipemenegotto@gmail.com>
2022-03-12 00:38:42 -0300

    Add files via upload

--- lula.py
+++ lula.py
@@ -1,17 +1,24 @@
-import os
+from os import read, write
 import tweepy
 import time
+import random
+import csv
+import pathlib
 
 api_key = os.environ['api_key']
 api_secret_key = os.environ['api_secret_key']
 access_key = os.environ['access_token']
 access_secret = os.environ['access_secret']
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
-auth.set_access_token(access_key, access_secret)
+auth.set_access_token(acess_key, acess_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+path_2 = str(pathlib.Path(__file__).parent.resolve())
+slash_2 = '\\'
+archive_name_2 = "last_seen.txt"
+full_path_2 = path_2 + slash_2 + archive_name_2
+FILE_NAME = full_path_2
 
-FILE_NAME = 'last_seen.txt'
 
 def read_last_seen(FILE_NAME):
     file_read = open(FILE_NAME, 'r')
@@ -25,17 +32,50 @@ def store_last_seen(FILE_NAME, last_seen_id):
     file_write.close()
     return
 
+def phrase(text):
+    global dict_lula
+    dict_aux = dict_lula
+    print(dict_aux)
+    list_of_words = []
+    list_of_words = text.split()
+    print(list_of_words)
+    link_to_post = 'Nada a declarar'
+    for e in list_of_words:
+        if e in dict_aux:
+            elemento = str(e)
+            link_to_post = dict_aux.get(elemento)
+            print(link_to_post)
+            break
+    return link_to_post    
+    
 def _main_():
     read_last_seen_str = str(read_last_seen(FILE_NAME))
-    tweets = api.search('Lula -filter:retweets', since_id =  read_last_seen_str, lang = 'pt')
-    print('Último ID pesquisado:' + read_last_seen_str)
+    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode = 'extended')
+    print('Ultimo ID pesquisado:' + read_last_seen_str)
     for tweet in reversed(tweets):
-        print(tweet.text)
-        api.create_friendship(tweet.user.screen_name)
-        api.retweet(tweet.id)
         store_last_seen(FILE_NAME, tweet.id)
+        text_tweet = tweet.full_text
+        text_tweet = text_tweet.lower()
+        print(text_tweet)
+        response = phrase(text_tweet)
+        api.update_status('@'+ tweet.user.screen_name + ' ' + response, in_reply_to_status_id=tweet.id)
         
+    
+def lula_dictionary():
+    path = str(pathlib.Path(__file__).parent.resolve())
+    print(path)
+    slash = '\\'
+    archive_name = "lula_dictionary.csv"
+    full_path = path + slash + archive_name
 
+    with open(full_path, mode='r') as infile:
+        reader = csv.reader(infile)
+        with open('coors_new.csv', mode='w') as outfile:
+            writer = csv.writer(outfile)
+            mydict = {rows[0]:rows[1] for rows in reader}
+    return mydict
+
+dict_lula = lula_dictionary()
 while True:
     _main_()
     time.sleep(60)
